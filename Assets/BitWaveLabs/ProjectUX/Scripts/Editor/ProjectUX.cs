a88f249ba3981ba9ef9b7f8cee766a5175e43785
celinedrules <celinedrules@hotmail.com>
2026-02-06 11:30:09 -0700

    Added `ProjectUX` framework initialization with folder structure and constants for database path.

---
+++ Assets/BitWaveLabs/ProjectUX/Scripts/Editor/ProjectUX.cs
@@ -0,0 +1,15 @@
+using UnityEditor;
+
+namespace BitWaveLabs.ProjectUX.Editor
+{
+    [InitializeOnLoad]
+    public static class ProjectUX
+    {
+        public const string DataBasePath = "Assets/BitWaveLabs/ProjectUX/Data/";
+
+        static ProjectUX()
+        {
+            
+        }
+    }
+}
